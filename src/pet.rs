2936b3bcc422cb78703eb57e1b03483421178a10
Ryan Gordon <ryangordon@duck.com>
2024-02-09 16:47:05 +1300

    Set up basic pet functionality

--- src/pet.rs
+++ src/pet.rs
@@ -104,8 +104,120 @@ impl Default for Pet {
 }
 
 impl Pet {
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+    
+    pub fn get_max_boredom(&self) -> u8 {
+        self.max_boredom
+    }
+    
+    pub fn get_max_hunger(&self) -> u8 {
+        self.max_hunger
+    }
+    
+    pub fn get_max_health(&self) -> u16 {
+        self.max_health
+    }
+    
+    pub fn get_reason_for_death(self) -> String {
+        if self.health != 0 {
+            return String::from("I'm still alive :eye:");
+        }
+        
+        self.reason_for_death
+    }
+    
+    pub fn change_name(&mut self, new_name: String) {
+        if !new_name.trim().is_empty() {
+            self.name = new_name;
+        }
+    }
+    
+    pub fn get_status_report(&self) -> Status {
+        let boredom_status_message;
+        let hunger_status_message;
+        let health_status_message;
+        
+        if self.boredom <= self.boredom_limit {
+            boredom_status_message = String::from("happy");
+        } else if self.boredom < self.anger_limit {
+            boredom_status_message = String::from("bored");
+        } else {
+            boredom_status_message = String::from("angry");
+        }
+        
+        if self.hunger < self.max_hunger / 2 {
+            hunger_status_message = String::from("full");
+        } else if self.hunger <= self.hunger_limit {
+            hunger_status_message = String::from("hungry");
+        } else {
+            hunger_status_message = String::from("starving");
+        }
+        
+        if f32::from(self.health) >= f32::from(self.max_health) * 0.8 {
+            health_status_message = String::from("fighting fit");
+        } else if f32::from(self.health) >= f32::from(self.max_health) * 0.2 {
+            health_status_message = String::from("ok");
+        } else if self.health >= 1 {
+            health_status_message = String::from("sick");
+        } else {
+            health_status_message = String::from("dead");
+        }
+        
+        Status {
+            boredom: self.boredom,
+            boredom_percentage: (self.boredom / self.max_boredom) * 100,
+            boredom_status_message,
+            hunger: self.hunger,
+            hunger_percentage: (self.hunger / self.max_hunger) * 100,
+            hunger_status_message,
+            health: self.health,
+            health_percentage: u8::try_from((self.health / self.max_health) * 100).unwrap(),
+            health_status_message
+        }
+    }
+    
+    fn get_boredom_rate(&self) -> u8 {
+        if self.boredom > self.boredom_limit {
+            return self.boredom_rate * 2;
+        }
+        
+        self.boredom_rate
+    }
+    
+    fn get_hunger_rate(&self) -> u8 {
+        let mut rate: u8 = self.hunger_rate;
+        
+        if self.health <= self.max_health / 4 {
+            rate /= 2;
+        }
+        
+        if self.boredom > self.boredom_limit {
+            rate *= 2;
+            
+            if self.boredom > self.anger_limit {
+                rate *= 2;
+            }
+        }
+        
+        rate
+    }
+    
     pub fn eat(&mut self, cake: &Cake) {
         self.hunger = u8::try_from(std::cmp::max(0, usize::from(self.hunger) - cake.get_hunger_replenished())).ok().unwrap();
         self.health = u16::try_from(std::cmp::min(usize::from(self.max_health), usize::from(self.health) + cake.get_health_replenished())).ok().unwrap();
     }
+}
+
+pub struct Status {
+    boredom: u8,
+    boredom_percentage: u8,
+    boredom_status_message: String,
+    hunger: u8,
+    hunger_percentage: u8,
+    hunger_status_message: String,
+    health: u16,
+    health_percentage: u8,
+    health_status_message: String
 }
