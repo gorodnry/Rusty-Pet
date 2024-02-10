a54ff4afb663eae844c73928fd6a99cc5d18b81c
Ryan Gordon <ryangordon@duck.com>
2024-02-11 01:41:43 +1300

    Rustfmt

--- src/cake.rs
+++ src/cake.rs
@@ -3,26 +3,36 @@ pub struct Cake {
     name: String,
     hunger_replenished: usize,
     health_replenished: usize,
-    cost: u8
+    cost: u8,
 }
 
 impl Cake {
-    pub fn new(name: String, hunger_replenished: usize, health_replenished: usize, cost: u8) -> Cake {
-        Cake { name, hunger_replenished, health_replenished, cost }
+    pub fn new(
+        name: String,
+        hunger_replenished: usize,
+        health_replenished: usize,
+        cost: u8,
+    ) -> Cake {
+        Cake {
+            name,
+            hunger_replenished,
+            health_replenished,
+            cost,
+        }
     }
-    
+
     pub fn get_name(&self) -> &String {
         &self.name
     }
-    
+
     pub fn get_hunger_replenished(&self) -> usize {
         self.hunger_replenished
     }
-    
+
     pub fn get_health_replenished(&self) -> usize {
         self.health_replenished
     }
-    
+
     pub fn get_cost(&self) -> u8 {
         self.cost
     }
--- src/pet.rs
+++ src/pet.rs
@@ -20,7 +20,7 @@ const BASE_HUNGER_RATE: u8 = 4;
 enum Strength {
     Weak,
     Normal,
-    Strong
+    Strong,
 }
 
 #[derive(Debug)]
@@ -77,8 +77,13 @@ impl Default for Pet {
 
         Pet {
             name: String::new(),
-            image_type: IMAGE_TYPES.choose(&mut rand::thread_rng()).unwrap().to_string(),
-            tombstone_type: rand::thread_rng().gen_range(1..=NUMBER_OF_TOMBSTONE_VARIATIONS).to_string(),
+            image_type: IMAGE_TYPES
+                .choose(&mut rand::thread_rng())
+                .unwrap()
+                .to_string(),
+            tombstone_type: rand::thread_rng()
+                .gen_range(1..=NUMBER_OF_TOMBSTONE_VARIATIONS)
+                .to_string(),
 
             max_boredom: BASE_MAX_BOREDOM,
             max_hunger: BASE_MAX_HUNGER,
@@ -107,38 +112,38 @@ impl Pet {
     pub fn get_name(&self) -> &String {
         &self.name
     }
-    
+
     pub fn get_max_boredom(&self) -> u8 {
         self.max_boredom
     }
-    
+
     pub fn get_max_hunger(&self) -> u8 {
         self.max_hunger
     }
-    
+
     pub fn get_max_health(&self) -> u16 {
         self.max_health
     }
-    
+
     pub fn get_reason_for_death(self) -> String {
         if self.health != 0 {
             return String::from("I'm still alive :eye:");
         }
-        
+
         self.reason_for_death
     }
-    
+
     pub fn change_name(&mut self, new_name: String) {
         if !new_name.trim().is_empty() {
             self.name = new_name;
         }
     }
-    
+
     pub fn get_status_report(&self) -> Status {
         let boredom_status_message;
         let hunger_status_message;
         let health_status_message;
-        
+
         if self.boredom <= self.boredom_limit {
             boredom_status_message = String::from("happy");
         } else if self.boredom < self.anger_limit {
@@ -146,7 +151,7 @@ impl Pet {
         } else {
             boredom_status_message = String::from("angry");
         }
-        
+
         if self.hunger < self.max_hunger / 2 {
             hunger_status_message = String::from("full");
         } else if self.hunger <= self.hunger_limit {
@@ -154,7 +159,7 @@ impl Pet {
         } else {
             hunger_status_message = String::from("starving");
         }
-        
+
         if f32::from(self.health) >= f32::from(self.max_health) * 0.8 {
             health_status_message = String::from("fighting fit");
         } else if f32::from(self.health) >= f32::from(self.max_health) * 0.2 {
@@ -164,7 +169,7 @@ impl Pet {
         } else {
             health_status_message = String::from("dead");
         }
-        
+
         Status {
             boredom: self.boredom,
             boredom_percentage: (self.boredom / self.max_boredom) * 100,
@@ -174,39 +179,49 @@ impl Pet {
             hunger_status_message,
             health: self.health,
             health_percentage: u8::try_from((self.health / self.max_health) * 100).unwrap(),
-            health_status_message
+            health_status_message,
         }
     }
-    
+
     fn get_boredom_rate(&self) -> u8 {
         if self.boredom > self.boredom_limit {
             return self.boredom_rate * 2;
         }
-        
+
         self.boredom_rate
     }
-    
+
     fn get_hunger_rate(&self) -> u8 {
         let mut rate: u8 = self.hunger_rate;
-        
+
         if self.health <= self.max_health / 4 {
             rate /= 2;
         }
-        
+
         if self.boredom > self.boredom_limit {
             rate *= 2;
-            
+
             if self.boredom > self.anger_limit {
                 rate *= 2;
             }
         }
-        
+
         rate
     }
-    
+
     pub fn eat(&mut self, cake: &Cake) {
-        self.hunger = u8::try_from(std::cmp::max(0, usize::from(self.hunger) - cake.get_hunger_replenished())).ok().unwrap();
-        self.health = u16::try_from(std::cmp::min(usize::from(self.max_health), usize::from(self.health) + cake.get_health_replenished())).ok().unwrap();
+        self.hunger = u8::try_from(std::cmp::max(
+            0,
+            usize::from(self.hunger) - cake.get_hunger_replenished(),
+        ))
+        .ok()
+        .unwrap();
+        self.health = u16::try_from(std::cmp::min(
+            usize::from(self.max_health),
+            usize::from(self.health) + cake.get_health_replenished(),
+        ))
+        .ok()
+        .unwrap();
     }
 }
 
@@ -219,5 +234,5 @@ pub struct Status {
     hunger_status_message: String,
     health: u16,
     health_percentage: u8,
-    health_status_message: String
+    health_status_message: String,
 }
